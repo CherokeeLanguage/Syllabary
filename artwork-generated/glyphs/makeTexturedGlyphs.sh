efa29d646d94df4a82c33f059967b381ac769bb4
mjoyner <mjoyner@vbservices.net>
2015-03-27 22:42:39 -0400

    new "marble" textured glyphs

---
+++ artwork-generated/glyphs/makeTexturedGlyphs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")" || exit 1
+
+#chmod +x graphics_utf
+
+DEST=textured
+if [ -d "${DEST}" ]; then
+	rm -rfv "${DEST}"
+fi
+mkdir "${DEST}"
+
+#inkscape -z -e "test.png" -d 90 -D test.svg
+
+for x in $(seq 5024 5108); do
+	h=$(printf "%04x" "$x")
+	i=$(printf "%04x" "$(($x+1))")
+	glyph="$(echo -e '\u'${h})"
+	ix=0
+	for svg in "textured-svg-src"/*.svg; do
+		cat "${svg}" | sed "s/Ꮝ/${glyph}/g" > "${DEST}/${h}_${ix}.svg"
+		inkscape -z -e "${DEST}/${h}_${ix}.png" -d 90 -D "${DEST}/${h}_${ix}.svg"
+		rm "${DEST}/${h}_${ix}.svg"
+		gm mogrify -trim -scale 128x128 "${DEST}/${h}_${ix}.png"
+		ix=$(($ix+1))
+	done
+done
