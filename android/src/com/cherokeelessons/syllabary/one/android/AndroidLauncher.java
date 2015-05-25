f1ef77cb620947fe2a4394995abb8bc828b6d0f6
mjoyner <mjoyner@vbservices.net>
2015-05-24 20:32:21 -0400

    xxx

--- android/src/com/cherokeelessons/syllabary/one/android/AndroidLauncher.java
+++ android/src/com/cherokeelessons/syllabary/one/android/AndroidLauncher.java
@@ -14,6 +14,11 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.hideStatusBar=true;
+		config.useImmersiveMode=true;
+		config.useAccelerometer=false;
+		config.useCompass=false;
+		config.useWakelock=false;
 		initialize(new SyllabaryApp(), config);
 		Platform.application=this;
 		App.services=new GameServices(App.CredentialsFolder, new Platform());
