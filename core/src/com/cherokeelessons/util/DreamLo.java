9433610868687fed7c1dec4fd40006e68b0a08f6
mjoyner <mjoyner@vbservices.net>
2016-04-02 13:20:30 -0400

    x

--- core/src/com/cherokeelessons/util/DreamLo.java
+++ core/src/com/cherokeelessons/util/DreamLo.java
@@ -1,7 +1,6 @@
 package com.cherokeelessons.util;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
