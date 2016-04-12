dc0bb1f7c2cdc35342fb76b833a2bf10082809c0
mjoyner <mjoyner@vbservices.net>
2016-04-11 20:07:42 -0400

    xxx

--- android/src/com/cherokeelessons/syllabary/one/android/AndroidLauncher.java
+++ android/src/com/cherokeelessons/syllabary/one/android/AndroidLauncher.java
@@ -1,7 +1,5 @@
 package com.cherokeelessons.syllabary.one.android;
 
-import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.cherokeelessons.play.GameServices;
@@ -9,16 +7,20 @@ import com.cherokeelessons.play.Platform;
 import com.cherokeelessons.syllabary.one.App;
 import com.cherokeelessons.syllabary.one.SyllabaryApp;
 
+import android.os.Bundle;
+
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.hideStatusBar=true;
-		config.useImmersiveMode=true;
-		config.useAccelerometer=false;
-		config.useCompass=false;
-		config.useWakelock=false;
+//		config.hideStatusBar=true;
+//		config.useImmersiveMode=true;
+//		config.useAccelerometer=false;
+//		config.useCompass=false;
+//		config.useWakelock=false;
+		config.maxSimultaneousSounds=8;
+//		config.resolutionStrategy= new RatioResolutionStrategy(1280f,720f);
 		initialize(new SyllabaryApp(), config);
 		Platform.application=this;
 		App.services=new GameServices(App.CredentialsFolder, new Platform());
