5a60763637af791d1e68cd8dba3c54ed87d2a50b
mjoyner <mjoyner@vbservices.net>
2018-04-03 20:22:34 -0400

    remove ACRA

--- android/src/com/cherokeelessons/syllabary/one/android/AndroidApplication.java
+++ android/src/com/cherokeelessons/syllabary/one/android/AndroidApplication.java
@@ -1,19 +1,13 @@
 package com.cherokeelessons.syllabary.one.android;
 
-import org.acra.ACRA;
-import org.acra.ReportingInteractionMode;
-import org.acra.annotation.ReportsCrashes;
-import org.acra.sender.HttpSender.Method;
-
 import android.app.Application;
-import android.util.Log;
 
-@ReportsCrashes(formUri = "http://www.cherokeelessons.com/acra.php", sendReportsAtShutdown = true, sendReportsInDevMode = true, disableSSLCertValidation = true, httpMethod = Method.POST, mode = ReportingInteractionMode.SILENT)
+//@ReportsCrashes(formUri = "http://www.cherokeelessons.com/acra.php", sendReportsAtShutdown = true, sendReportsInDevMode = true, disableSSLCertValidation = true, httpMethod = Method.POST, mode = ReportingInteractionMode.SILENT)
 public class AndroidApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		ACRA.init(this);
-		Log.d(this.getClass().getName(), "ACRA.init#done");
+//		ACRA.init(this);
+//		Log.d(this.getClass().getName(), "ACRA.init#done");
 	}
 }
